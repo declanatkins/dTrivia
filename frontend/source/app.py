0643f428ba1375a7ea1cf04174a34a62c4086417
Declan Atkins <declanatkins@gmail.com>
2022-12-22 16:45:26 +0000

    Added login logout

--- frontend/source/app.py
+++ frontend/source/app.py
@@ -1,13 +1,16 @@
+from functools import wraps
 from flask import (
     Flask,
     render_template,
     request,
     redirect,
     url_for,
-    flash
+    flash,
+    session
 )
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from settings import get_settings
+from utils import login_required, make_backend_request
 
 
 app = Flask(__name__)
@@ -17,3 +20,22 @@ settings = get_settings()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    response = make_backend_request('post', 'login', data)
+    if response.status_code == 200:
+        session['session_id'] = response.json()['session_id']
+    else:
+        flash('Invalid Credentials')
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    make_backend_request('post', 'logout')
+    session.pop('session_id')
+    return redirect(url_for('index'))
