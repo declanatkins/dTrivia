7f0e6a35ad9876fd650a60ea651652eb0dc115bd
Declan Atkins <declanatkins@gmail.com>
2022-12-22 17:05:35 +0000

    Added login and register pages

--- frontend/source/app.py
+++ frontend/source/app.py
@@ -22,10 +22,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    response = make_backend_request('post', 'login', data)
+    response = make_backend_request('post', 'users/login', data)
     if response.status_code == 200:
         session['session_id'] = response.json()['session_id']
     else:
@@ -33,9 +38,25 @@ def login():
     return redirect(url_for('index'))
 
 
-@app.route('/logout')
+@app.route('/logout', methods=['POST'])
 @login_required
 def logout():
-    make_backend_request('post', 'logout')
+    make_backend_request('post', 'users/logout')
     session.pop('session_id')
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET'])
+def register_page():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    response = make_backend_request('post', 'users/', data)
+    if response.status_code == 200:
+        flash('User created successfully')
+    else:
+        flash('Invalid Credentials')
     return redirect(url_for('index'))
